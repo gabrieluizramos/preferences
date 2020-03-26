dee2875c1a5ce820c1661a826b4cebad12843ba0
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2020-03-26 10:20:51 -0300

    chore(variables): update color variables

--- variables/colors.js
+++ variables/colors.js
@@ -3,12 +3,16 @@ export default {
   black: '#000',
   terminal: {
     blue: {
-      default: '#006cad',
-      light: '#4f93d5'
+      default: '#006CAD',
+      light: '#4F93D5'
     },
-    purple: '#696ac6',
-    green: '#4fb06e',
-    yellow: '#f8ab03',
-    black: '#1f1e1e'
+    purple: '#696AC6',
+    green: '#4FB06E',
+    yellow: '#F8AB03',
+    black: '#1F1E1E'
+  },
+  post: {
+    color: '#DADADA',
+    background: '#1F1F1F'
   }
 }
