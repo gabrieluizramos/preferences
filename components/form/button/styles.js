1ff8e49d0c3072d288d68bfbc8331b97cf8c7e18
gabrieluizramos <gabriel.luiz.ramos@gmail.com>
2020-04-29 10:17:07 -0300

    feat(form): add form to preferences

---
+++ components/form/button/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+import { spacing, font, colors } from '../../../variables';
+
+export const Button = styled.button`
+  cursor: pointer;
+  padding: ${spacing.default};
+  font-size: ${font.size.default};
+  transition: .2s ease-in-out;
+  border: 2px solid ${colors.white};
+
+  &:not([disabled]) {
+    &:hover {
+      background: transparent;
+      color: ${colors.white};
+    }
+  }
+
+  &[disabled] {
+    cursor: initial;
+    background: ${colors.post.color};
+  }
+`;
--- components/index.js
+++ components/index.js
@@ -4,3 +4,4 @@ export { default as Wrapper } from './wrapper';
 export { default as Header } from './header';
 export { default as Footer } from './footer';
 export { default as Link } from './link';
+export { default as Form } from './form';
